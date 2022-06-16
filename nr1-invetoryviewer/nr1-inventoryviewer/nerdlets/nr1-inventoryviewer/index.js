5eb3c016711f85c71b5d9579ac29d799e840d7cc
Anthony Nguyen <nvhoanganh1909@gmail.com>
2022-06-16 10:36:07 +1000

    rename

--- nr1-invetoryviewer/nr1-inventoryviewer/nerdlets/nr1-inventoryviewer/index.js
+++ nr1-invetoryviewer/nr1-inventoryviewer/nerdlets/nr1-inventoryviewer/index.js
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { PlatformStateContext, NerdletStateContext, nerdlet, Icon } from 'nr1';
 
-// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+const DataContext = React.createContext();
 
-export default class Nr1InventoryviewerNerdlet extends React.Component {
-  render() {
-    return <h1>Hello, nr1-inventoryviewer Nerdlet!</h1>;
-  }
+function NerdletRoot() {
+  useEffect(() => {
+    nerdlet.setConfig({
+      accountPicker: false,
+      timePicker: false,
+      actionControls: true,
+      actionControlButtons: [
+        {
+          label: 'New Issue',
+          type: 'secondary',
+          iconType: Icon.TYPE.INTERFACE__SIGN__EXCLAMATION,
+          onClick: () =>
+            window.open(
+              'https://github.com/newrelic-experimental/nr1-gaps/issues/new?assignees=&labels=bug%2C+needs-triage&template=bug_report.md&title=',
+              '_blank'
+            )
+        },
+        {
+          label: 'Feature Request',
+          type: 'secondary',
+          iconType: Icon.TYPE.PROFILES__EVENTS__FAVORITE__WEIGHT_BOLD,
+          onClick: () =>
+            window.open(
+              'https://github.com/newrelic-experimental/nr1-gaps/issues/new?assignees=&labels=enhancement%2C+needs-triage&template=enhancement.md&title=',
+              '_blank'
+            )
+        }
+      ]
+    });
+  }, []);
+
+  const platformContext = useContext(PlatformStateContext);
+  const nerdletContext = useContext(NerdletStateContext);
+
+  return (
+    <div>
+      <DataProvider {...platformContext} {...nerdletContext}>
+        <div>Hello world!!</div>
+      </DataProvider>
+    </div>
+  );
 }
+
+
+function DataProvider({ children }) {
+  const [rootState, setRootState] = useState({});
+  return (
+    <DataContext.Provider
+      value={{
+        ...rootState,
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+}
+
+export default NerdletRoot;
